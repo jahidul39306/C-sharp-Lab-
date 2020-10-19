008f2a0cc8d5e469c0f675334bc5357691b3cb59
jahidulananto <jahidulislamananto@gmail.com>
2020-10-19 13:20:48 +0600

    Lab Task 2 Complete

--- Lab Task 2/LabTask_2/Program.cs
+++ Lab Task 2/LabTask_2/Program.cs
@@ -8,6 +8,7 @@ namespace LabTask_2
         static void Main(string[] args)
         {
             // 1
+            Console.WriteLine("Number 1:");
             int n = 50;
             for (int i = 1; i <= n; i++)
             {
@@ -18,9 +19,17 @@ namespace LabTask_2
             }
 
             // 2
-
-
+            Console.WriteLine("\nNumber 2:");
+            string a = "X0X0X0";
+            string b = "0X0X0X";
+            for (int i = 1; i < 5; i++)
+            {
+                if (i % 2 == 0) { Console.WriteLine(b); }
+                else Console.WriteLine(a);
+            }
+            
             // 3
+            Console.WriteLine("\nNumber 3:");
             for (int i = 1; i < 6; i++)
             {
                 for (int j = i; j < i + 5; j++)
@@ -29,8 +38,9 @@ namespace LabTask_2
                 }
                 Console.WriteLine();
             }
-
+            
             // 4
+            Console.WriteLine("\nNumber 4:");
             int odd = 0;
             int even = 0;
             for (int i = 1; i <= 100; i++)
@@ -42,6 +52,7 @@ namespace LabTask_2
             Console.WriteLine("Sum of even number = " + even);
 
             // 5
+            Console.WriteLine("\nNumber 5:");
             for (int i = 1; i < 6; i++)
             {
                 for (int j = 1; j < i; j++) { Console.Write(" "); }
@@ -49,6 +60,7 @@ namespace LabTask_2
             }
 
             // 6
+            Console.WriteLine("\nNumber 6:");
             int m = 5;
             int factorial = 1;
             for (int i = 1; i <= m; i++)
@@ -56,6 +68,26 @@ namespace LabTask_2
                 factorial = factorial * i;
             }
             Console.WriteLine("Factorial is " + factorial);
+
+            // 7
+            Console.WriteLine("\nNumber 7:");
+            int p = 5;
+            for (int i = 1; i <= p; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int i = p - 1; i > 0; i--)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
